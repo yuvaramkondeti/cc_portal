eac1207a892261c170fc5faf5d51008a631f6090
Kondeti, Yuvaram <yuvaram.kondeti@everi.com>
2024-09-30 13:09:52 +0000

    Updated prod.env.js

--- cruise-control-ui/config/prod.env.js
+++ cruise-control-ui/config/prod.env.js
@@ -1,5 +1,8 @@
 /* Copyright 2017-2019 LinkedIn Corp. Licensed under the BSD 2-Clause License (the "License"). See License in the project root for license information. */
 
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  VUE_APP_USERNAME: '"user"',
+  VUE_APP_PASSWORD: '"YOUR_PASSWORD_HERE"',
+  VUE_APP_IDLE_TIME: '"1200000"'
 }
