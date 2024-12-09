20fb95cefffa5beebfdfc249189a3b4ea5c619d0
yuvaramkondeti <yuvaram.kondeti@everi.com>
2024-12-09 15:51:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,24 +2,30 @@ import os
 import subprocess
 
 if __name__ == "__main__":
-    rootDir = os.getcwd();
-    # Change the directory to the folder where your app is located
-    cruise_node_dir = os.path.join(os.getcwd(), "cruise-control-node")  # Path to the 'cruise_node_dir' folder
+    rootDir = os.getcwd()  # Get the current working directory
+    
+    # Path to the 'cruise-control-node' folder
+    cruise_node_dir = os.path.join(rootDir, "cruise-control-node")
 
-    # Ensure the React app folder exists
+    # Run Gradle build
     subprocess.run(["./gradlew", "build"], check=True)
-    if os.path.isdir(cruise_node_dir):
 
+    # Ensure the cruise_node_dir exists
+    if os.path.isdir(cruise_node_dir):
         # Change to the cruise_node_dir directory
         os.chdir(cruise_node_dir)
 
-        # Install dependencies (npm install)
+        # Install dependencies
         subprocess.run(["npm", "install", "--force"], check=True)
 
-        # Run npm start to start the React development server
+        # Run the React app server (assuming this is what you want to do)
         subprocess.run(["node", "server.js"], check=True)
-	os.chdir(rootDir);
-	subprocess.Popen(["nohup", "sh", "kafka-cruise-control-start.sh", "config/cruisecontrol.properties", "&"], check=True)
+        
+        # Change back to the original root directory
+        os.chdir(rootDir)
+
+        # Run Kafka Cruise Control start script in the background using subprocess.Popen
+        subprocess.Popen(["nohup", "sh", "kafka-cruise-control-start.sh", "config/cruisecontrol.properties", "&"])
+
     else:
         print(f"Error: The directory '{cruise_node_dir}' does not exist.")
-
