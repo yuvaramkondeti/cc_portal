dce7d2806e4dd92a68f1e4480a45502a9e17a9b7
yuvaramkondeti <yuvaram.kondeti@everi.com>
2024-12-17 09:51:41 +0530

    Add files via upload

--- cruise-control-ui/config/prod.env.js
+++ cruise-control-ui/config/prod.env.js
@@ -6,5 +6,9 @@ module.exports = {
   VUE_APP_PASSWORD: '"Kafka@2024"',
   VUE_APP_IDLE_TIME: '"1200000"',
   VUE_APP_DEV_USERNAME: '"developer"',
-  VUE_APP_DEV_PASSWORD: '"Dev@2024"'
+  VUE_APP_DEV_PASSWORD: '"Dev@2024"',
+  VUE_GIT_CLIENT_ID:'"Ov23liJOxDVscOlOO9vo"',
+  VUE_GIT_CLIENT_SECRET: '"53dae2818c7b45ba1a5596a1949451de6159c3be"',
+  VUE_GIT_REDIRECT_URI: '"http://maaappxvw01t.everi.com:9090/#/auth/callback"',
+  VUE_CC_ADMIN_EMAILS: '"yuvaram.kondeti@everi.com,shivani.dandu@everi.com,vandan.manchella@everi.com,kali.dutt@everi.com"'
 }
